01236975642b634900bf9372f18a05dd4e7b00fc
Isa <94567698+isa-omarov123@users.noreply.github.com>
2022-12-02 16:14:32 +0300

    Update compile_nuitka.bat

--- Virus/compile_nuitka.bat
+++ Virus/compile_nuitka.bat
@@ -2,7 +2,7 @@
 
 python -m nuitka --windows-disable-console --follow-imports --windows-icon-from-ico=icon.ico "VirusV4.1.py"
 
-rmdir /s /q VirusV4.build
+rmdir /s /q VirusV4.1.build
 del "VirusV4.1.cmd"
 
 :cmd
